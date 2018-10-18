72a2da158b9e7a61ded45a0596ea05bc65f6210a
Itzel Pérez Beltrán <itzelperezb@macbook-air-de-itzel.local>
2018-10-18 18:45:19 -0500

    Outlet de etiqueta y action de botón agregados.

--- ejercicioGit/ViewController.swift
+++ ejercicioGit/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelContador: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func botonAgregar(_ sender: UIButton) {
+        
+    }
+    
 
 }
 
